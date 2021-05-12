12674680532e4551b486d8999ffe70c3f7603446
theArjun <be2017se573@gces.edu.np>
2021-05-12 15:48:53 +0545

    Create root reducer

--- src/store.js
+++ src/store.js
@@ -1,17 +1,5 @@
 import { createStore } from 'redux'
+import rootReducer from './reducers/index'
 
-const initialState = {
-  sidebarShow: 'responsive'
-}
-
-const changeState = (state = initialState, { type, ...rest }) => {
-  switch (type) {
-    case 'set':
-      return {...state, ...rest }
-    default:
-      return state
-  }
-}
-
-const store = createStore(changeState)
+const store = createStore(rootReducer)
 export default store
