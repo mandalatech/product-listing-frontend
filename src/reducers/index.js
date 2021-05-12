93972c2238e9180a0b8e7b0cb5db3e372f39633f
theArjun <be2017se573@gces.edu.np>
2021-05-12 17:59:29 +0545

    Fallback to basic setup

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,11 @@
+import * as actionTypes from './actions'
+
 const initialState = {
   sidebarShow: 'responsive',
 }
 
 const rootReducer = (state = initialState, { type, ...rest }) => {
-  switch (type) {
-    case 'set':
-      return { ...state, ...rest }
-    default:
-      return state
-  }
+  return state
 }
 
 export default rootReducer
