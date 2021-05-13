e71f634e6bc3efd7d7d3f8cec179ef36a03b65cd
theArjun <be2017se573@gces.edu.np>
2021-05-13 17:10:10 +0545

    Add Meta Description

---
+++ src/views/ProductDataEntry/MetaDescription/index.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { CCol, CRow, CCardBody, CCard, CFormGroup } from '@coreui/react'
+import { CIcon } from '@coreui/icons-react'
+import { cilInfo } from '@coreui/icons'
+
+import TextArea from '../../components/TextArea'
+import TextField from '../../components/TextField'
+
+const MetaDescription = () => {
+  return (
+    <>
+      <CRow>
+        <CCol xs="12" md="12" className="mb-4">
+          <h4 className="outside-card-title mb-4">
+            Meta Description (Optional)
+          </h4>
+          <CCard className="addpro-custom-card sm-pd">
+            <CCardBody>
+              <CFormGroup className="mb-4">
+                <TextField
+                  name="metaTitle"
+                  label="Title"
+                  placeholder="Meta title of product"
+                />
+                <TextArea
+                  name="metaDescription"
+                  label="Description"
+                  placeholder="Short meta description about product"
+                />
+                <TextField
+                  name="metaKeywords"
+                  label="Keywords"
+                  placeholder="Keyword e.g. furniture, wood, chair"
+                />
+
+                <div>
+                  <CIcon name="cilSettings" />
+                  &nbsp;
+                  <span>Add multiple keyword seperated by “,” comma</span>
+                </div>
+              </CFormGroup>
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+    </>
+  )
+}
+
+export default MetaDescription
--- src/views/ProductDataEntry/index.js
+++ src/views/ProductDataEntry/index.js
@@ -4,6 +4,7 @@ import AddProduct from './BasicInfo/AddProduct'
 import Description from './Description'
 import InventoryControl from './InventoryControl'
 import Measurement from './Measurement'
+import MetaDescription from './MetaDescription'
 
 const DataEntry = () => {
   return (
@@ -12,6 +13,7 @@ const DataEntry = () => {
       <InventoryControl />
       <Description/>
       <Measurement/>
+      <MetaDescription/>
     </>
   )
 }
