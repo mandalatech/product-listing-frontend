193ceb8f9c6857e47d9d61acae0aea77193d43bc
theArjun <be2017se573@gces.edu.np>
2021-05-13 14:00:35 +0545

    Populate data for Product Group, Manufacturer & Brand

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,8 +4,26 @@ const initialState = {
   sidebarShow: 'responsive',
 }
 
-const rootReducer = (state = initialState, { type, ...rest }) => {
-  return state
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.UPDATE_PRODUCT_GROUP:
+      return {
+        ...state,
+        productGroups: action.payload,
+      }
+    case actionTypes.UPDATE_BRANDS:
+      return {
+        ...state,
+        brands: action.payload,
+      }
+    case actionTypes.UPDATE_MANUFACTURERS:
+      return {
+        ...state,
+        manufacturers: action.payload,
+      }
+    default:
+      return state
+  }
 }
 
 export default rootReducer
--- src/views/allProducts/addProduct/Configurable.js
+++ src/views/allProducts/addProduct/Configurable.js
@@ -1,10 +1,32 @@
-import React from "react";
-import { CCol, CRow, CFormGroup } from "@coreui/react";
+import React, { useEffect } from 'react'
+import { CCol, CRow, CFormGroup } from '@coreui/react'
+import { connect } from 'react-redux'
 
-import TextField from "../../components/TextField";
-import DropdownTextField from "../../components/DropDownTextField";
+import TextField from '../../components/TextField'
+import DropdownTextField from '../../components/DropDownTextField'
 
-const Configurable = () => {
+import {
+  PRODUCT_GROUP_URL,
+  BRAND_URL,
+  MANUFACTURER_URL,
+} from './../../../constants/urls'
+import * as actionTypes from './../../../reducers/actions'
+
+import callAPI from './../../../api'
+
+const Configurable = (props) => {
+  useEffect(() => {
+    callAPI(PRODUCT_GROUP_URL, 'get').then((res) => {
+      props.updateProductGroups(res)
+    })
+    callAPI(BRAND_URL, 'get').then((res) => {
+      props.updateBrands(res)
+    })
+    callAPI(MANUFACTURER_URL, 'get').then((res) => {
+      props.updateManufacturers(res)
+    })
+  }, [])
+
   return (
     <>
       <CFormGroup className="mb-4">
@@ -21,7 +43,7 @@ const Configurable = () => {
             name="group"
             label="Group"
             placeholder="Select Group"
-            // options
+            options={props.productGroups}
           />
         </CCol>
         <CCol xs="4">
@@ -29,19 +51,48 @@ const Configurable = () => {
             name="manufacturer"
             label="Manufacturer"
             placeholder="Select Manufacturer"
-            // options
+            options={props.manufacturers}
           />
         </CCol>
         <CCol xs="4">
-          <TextField
+          <DropdownTextField
             name="configBrand"
             label="Brand"
             placeholder="Enter brand name"
+            options={props.brands}
           />
         </CCol>
       </CRow>
     </>
-  );
-};
+  )
+}
 
-export default Configurable;
+const mapStateToProps = (state) => {
+  return {
+    productGroups: state.productGroups,
+    brands: state.brands,
+    manufacturers: state.manufacturers,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateProductGroups: (groups) =>
+      dispatch({
+        type: actionTypes.UPDATE_PRODUCT_GROUP,
+        payload: groups,
+      }),
+    updateBrands: (groups) =>
+      dispatch({
+        type: actionTypes.UPDATE_BRANDS,
+        payload: groups,
+      }),
+    updateManufacturers: (groups) =>
+      dispatch({
+        type: actionTypes.UPDATE_MANUFACTURERS,
+        payload: groups,
+      }),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Configurable)
