9925bdeeadbb627f7effad76ac5b724d0986304f
theArjun <be2017se573@gces.edu.np>
2021-05-13 16:42:00 +0545

    Add switch component

---
+++ src/views/components/Switch.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { CSwitch } from '@coreui/react'
+
+const Switch = ({ leftLabel, rightLabel, color, labelOn, labelOff }) => {
+  return (
+    <div className="d-flex align-items-center">
+      <span>
+        <b>{labelOn}</b>
+      </span>
+      &nbsp; &nbsp;
+      <CSwitch
+        color={color}
+        checked
+        value={color}
+        shape="pill"
+        labelOn={labelOn}
+        labelOff={labelOff}
+      />
+      &nbsp; &nbsp;
+      <span>
+        <b>{labelOff}</b>
+      </span>
+    </div>
+  )
+}
+
+export default Switch
