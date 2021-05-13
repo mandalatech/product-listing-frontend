2f60b4a825fc4e35bb65d27f728ae329038821b1
theArjun <be2017se573@gces.edu.np>
2021-05-13 15:30:10 +0545

    Update data entry page

--- src/views/ProductDataEntry/index.js
+++ src/views/ProductDataEntry/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
 import AddProduct from './BasicInfo/AddProduct'
-import InventoryControl from './inventoryControl/InventoryControl'
+import Description from './Description'
+import InventoryControl from './InventoryControl/InventoryControl'
 
 const DataEntry = () => {
   return (
     <>
       <AddProduct />
       <InventoryControl />
+      <Description/>
     </>
   )
 }
