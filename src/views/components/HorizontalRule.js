dc7d0a4b0caee7b6e2e15071fdb8fc7801ae2fa6
theArjun <be2017se573@gces.edu.np>
2021-05-17 16:18:12 +0545

    Customize `HorizontalRule` component

--- src/views/components/HorizontalRule.js
+++ src/views/components/HorizontalRule.js
@@ -1,18 +1,23 @@
 import React from 'react'
 
-const HorizontalRule = () => {
+const HorizontalRule = ({ marginBottom, marginTop }) => {
   const style = {
     width: '100%',
     height: 1,
     backgroundColor: 'rgba(0,0,0,0.125)',
-    marginBottom: '1.75rem',
-    marginTop: '1.75rem',
+    marginBottom: marginBottom,
+    marginTop: marginTop,
   }
   return (
     <div>
       <div style={style}></div>
     </div>
   )
+}
+
+HorizontalRule.defaultProps = {
+  marginBottom: '1.75rem',
+  marginTop: '1.75rem',
 }
 
 export default HorizontalRule
