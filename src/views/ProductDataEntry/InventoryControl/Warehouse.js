b1524005690a170926d36a236864ba3dc52bc1a1
theArjun <be2017se573@gces.edu.np>
2021-05-20 14:53:38 +0545

    Enhance Inventory Control UI

--- src/views/ProductDataEntry/InventoryControl/Warehouse.js
+++ src/views/ProductDataEntry/InventoryControl/Warehouse.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { CRow, CCol, CButton, CFormGroup, CLabel } from "@coreui/react";
-import CIcon from "@coreui/icons-react";
-import { freeSet } from "@coreui/icons";
+import React from 'react'
+import { CRow, CCol, CButton, CFormGroup, CLabel } from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { freeSet } from '@coreui/icons'
 
-import ComboInput from "../../components/ComboInput";
+import ComboInput from '../../components/ComboInput'
 
 const Warehouse = () => {
   return (
     <div>
       <div className="mb-4">
-        <h4 style={{ color: "black" }}>Warehouse</h4>
-        <p style={{ fontSize: 16, color: "rgba(0,0,0,0.5)" }}>
+        <h4 style={{ color: 'black' }}>Warehouse</h4>
+        <p style={{ fontSize: 16, color: 'rgba(0,0,0,0.5)' }}>
           Inventory can be set to multiple warehouses
         </p>
       </div>
@@ -33,38 +33,18 @@ const Warehouse = () => {
             // options
           />
         </CCol>
-
-        <CCol xs="4">
-          <CFormGroup style={{ marginBottom: 13 }}>
-            <CLabel></CLabel>
-          </CFormGroup>
-
-          <CRow>
-            <CCol col="6" sm="4" md="2" xl className="mb-3 mb-xl-0">
-              <CButton block color="dark">
-                Save
-              </CButton>
-            </CCol>
-
-            <CCol col="6" sm="4" md="2" xl className="mb-3 mb-xl-0">
-              <CButton block variant="outline" color="dark">
-                Cancel
-              </CButton>
-            </CCol>
-          </CRow>
-        </CCol>
       </CRow>
 
-      <div style={{ display: "flex" }}>
+      <div style={{ display: 'flex' }}>
         <div style={{ marginRight: 14 }}>
-          <CIcon content={freeSet.cilPlus} size={"lg"} />
+          <CIcon content={freeSet.cilPlus} size={'lg'} />
         </div>
-        <div style={{ display: "flex", alignItems: "center" }}>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
           <CLabel className="mb-0">Add Another Warehouse</CLabel>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Warehouse;
+export default Warehouse
