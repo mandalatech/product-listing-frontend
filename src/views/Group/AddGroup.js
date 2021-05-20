1617020353f35b021a33eee0353c04c24bceafb3
theArjun <be2017se573@gces.edu.np>
2021-05-20 18:03:27 +0545

    Make group attribute dynamic

--- src/views/Group/AddGroup.js
+++ src/views/Group/AddGroup.js
@@ -3,7 +3,7 @@ import { CCol, CRow, CCardBody, CCard, CButton } from '@coreui/react'
 import { CIcon } from '@coreui/icons-react'
 
 import TextField from 'src/views/components/TextField'
-
+import GroupContainer from './GroupContainer'
 const AddGroup = () => {
   return (
     <>
@@ -11,19 +11,8 @@ const AddGroup = () => {
         <h4 className="outside-card-title mb-4 p-2">Add Group</h4>
         <CCardBody>
           <TextField label="Group name" placeholder="Enter group name here" />
-          <CRow>
-            <CCol md="6">
-              <TextField label="Attribute" placeholder="Eg. Hard-disk" />
-            </CCol>
-            <CCol md="6">
-              <TextField label="Possible values" placeholder="Eg. SSD, HDD" />
-              <div>
-                <CIcon name="cilSettings" />
-                &nbsp;
-                <span>Add multiple values seperated by comma ‘,’</span>
-              </div>
-            </CCol>
-          </CRow>
+
+          <GroupContainer />
 
           <CRow>
             <CCol sm="2" md="2">
