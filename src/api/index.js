1ad779df355f788e4f4e12f1c4e44061a39fc3e2
theArjun <be2017se573@gces.edu.np>
2021-05-20 14:28:14 +0545

    Add parameter for `contentType`

--- src/api/index.js
+++ src/api/index.js
@@ -5,11 +5,19 @@ const _axios = axios.create({
   baseURL: BASE_URL,
 })
 
-const callAPI = async (url, method = 'get', data) => {
+const callAPI = async (
+  url,
+  method = 'get',
+  data,
+  contentType = 'application/json'
+) => {
   return await _axios({
     method: method,
     url: url,
     data: data,
+    headers: {
+      'Content-Type': contentType,
+    },
   })
     .then((res) => res.data)
     .catch((err) => err)
