f2f578e2ca8c75b3f93f816a84ad6f136fcad524
theArjun <be2017se573@gces.edu.np>
2021-05-23 14:58:43 +0545

    Create Brand Data Table

--- src/views/Brand/index.js
+++ src/views/Brand/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import BrandHeader from './BrandHeader'
 import AddBrand from './AddBrand'
+import BrandDataTable from './BrandDataTable'
 
 const Brand = () => {
-    return (
-        <div>
-            <BrandHeader/>
-            <AddBrand/>
-        </div>
-    )
+  return (
+    <div>
+      <BrandHeader />
+      <AddBrand />
+      <h4 className="mt-5 mb-3 font-weight-bold">All Brands</h4>
+      <BrandDataTable />
+    </div>
+  )
 }
 
 export default Brand
