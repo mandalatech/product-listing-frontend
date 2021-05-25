dd0a60f2e3283ac6a7647545ddc7cc21925c04af
theArjun <be2017se573@gces.edu.np>
2021-05-25 17:02:17 +0545

    Make brand deleted from Modal

--- src/views/Brand/DeleteBrand.js
+++ src/views/Brand/DeleteBrand.js
@@ -2,32 +2,50 @@ import { CButton, CSpinner } from '@coreui/react'
 import React, { useState } from 'react'
 
 import { deleteBrand } from 'src/api/brandRequests'
-
 const DeleteBrand = ({ item }) => {
-  const [deleting, setDeleting] = useState(false)
+  // State of deletions.
+  const STATE = Object.freeze({
+    NOT_DELETED: 'NOT_DELETED',
+    DELETING: 'DELETING',
+    DELETED: 'DELETED',
+  })
+
+  const [deletion, setDeletion] = useState(STATE.NOT_DELETED)
 
   const deleteGroup = async (item) => {
     const abortController = new AbortController()
     const signal = abortController.signal
-    setDeleting(true)
+    setDeletion(STATE.DELETING)
     console.log('[DELETE] BRAND: ', item)
     const deleteResponse = await deleteBrand(signal, item.id)
-    setDeleting(false)
+    setDeletion(STATE.DELETED)
   }
 
   return (
     <div className="text-center">
-      <p>
-        Are you sure want to delete <strong>{item.name}</strong> brand ?
-      </p>
+      {deletion == STATE.DELETED ? (
+        <p>
+          <strong>{item.name}</strong> brand successfully deleted.
+        </p>
+      ) : (
+        <p>
+          Are you sure want to delete <strong>{item.name}</strong> brand ?
+        </p>
+      )}
       <CButton
-        color="danger"
-        disabled={deleting}
+        color={deletion == STATE.DELETED ? 'success' : 'danger'}
+        disabled={deletion == STATE.DELETING || deletion == STATE.DELETED}
         onClick={() => {
           deleteGroup(item)
         }}
       >
-        {deleting ? <CSpinner color="secondary" size="sm" /> : 'Delete'}
+        {deletion == STATE.DELETING ? (
+          <CSpinner color="secondary" size="sm" />
+        ) : deletion == STATE.NOT_DELETED ? (
+          'Delete'
+        ) : (
+          'Deleted'
+        )}
       </CButton>
     </div>
   )
