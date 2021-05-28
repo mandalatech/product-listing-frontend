96c301c8a2a5b803b91582a4a309105552cced7e
theArjun <be2017se573@gces.edu.np>
2021-05-29 00:40:10 +0545

    Allow to populated newly added dynamic product group attributes

--- src/views/ProductDataEntry/ProductGroupFields/AddProductGroupFields.js
+++ src/views/ProductDataEntry/ProductGroupFields/AddProductGroupFields.js
@@ -1,18 +1,24 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { CCardBody, CCard, CRow, CCol } from '@coreui/react'
 import ComboInput from 'src/views/components/ComboInput'
 import TextField from 'src/views/components/TextField'
 import { connect } from 'react-redux'
 
 import { changeProductExtrasInput } from 'src/reducers/actions/index'
+import isEmpty from 'src/validations/isEmpty'
+import HorizontalRule from 'src/views/components/HorizontalRule'
 
 const AddProductGroupFields = ({
   fields,
   title,
   edit,
   extrasWithAttributes,
+  productGroups,
+  currentProductGroupID,
   ...props
 }) => {
+  const [newlyAddedAttributes, setNewlyAddedAttributes] = useState([])
+
   // Generate options for checkbox
   const optionsForCheckbox = (arr) => {
     return arr.map((val, index) => {
@@ -27,10 +33,31 @@ const AddProductGroupFields = ({
   }
 
   useEffect(() => {
+    // Initial populating the fields from store to UI.
     extrasWithAttributes.forEach(({ value, attribute }) => {
       _setValuesForProductExtras(attribute.slug, value)
     })
   }, [extrasWithAttributes])
+
+  useEffect(() => {
+    const productGroup = productGroups.find(
+      (pg) => pg.id === currentProductGroupID
+    )
+    if (productGroup) {
+      // Get lists of attribute IDs that are already populated.
+      const currentlyPopulatedFieldIDs = extrasWithAttributes.map(
+        (extra) => extra.attribute.id
+      )
+      // Filter from the whole group of attributes that are not populated.
+      const unPopulatedAttributes = productGroup.fields.filter((field) => {
+        if (!currentlyPopulatedFieldIDs.includes(field.id)) {
+          return true
+        }
+        return false
+      })
+      setNewlyAddedAttributes(unPopulatedAttributes)
+    }
+  }, [extrasWithAttributes, productGroups])
 
   return (
     <CRow>
@@ -39,43 +66,90 @@ const AddProductGroupFields = ({
         <CCard className="addpro-custom-card sm-pd">
           <CCardBody>
             {edit ? (
-              <CRow>
-                {extrasWithAttributes.map(({ value, attribute }) => {
-                  return (
-                    <CCol xs="12" md="6" lg="4">
-                      {attribute.datatype === 'enum' ? (
-                        <ComboInput
-                          label={attribute.name}
-                          name={attribute.slug}
-                          placeholder={`Enter ${attribute.name}`}
-                          options={optionsForCheckbox(
-                            attribute.enum_group.values
+              <div>
+                <CRow>
+                  {extrasWithAttributes.map(({ value, attribute }) => {
+                    return (
+                      <CCol xs="12" md="6" lg="4">
+                        {attribute.datatype === 'enum' ? (
+                          <ComboInput
+                            label={attribute.name}
+                            name={attribute.slug}
+                            placeholder={`Enter ${attribute.name}`}
+                            options={optionsForCheckbox(
+                              attribute.enum_group.values
+                            )}
+                            // TODO Dont forget to clear the old values.
+                            onChange={(e) => {
+                              _setValuesForProductExtras(attribute.slug, e.name)
+                            }}
+                            selectValueByLabel={true}
+                            valueByLabel={props.extras[attribute.slug]}
+                          />
+                        ) : (
+                          <TextField
+                            label={attribute.name}
+                            name={attribute.slug}
+                            placeholder={`Enter ${attribute.name}`}
+                            onChange={(e) => {
+                              _setValuesForProductExtras(
+                                attribute.slug,
+                                e.target.value
+                              )
+                            }}
+                            value={props.extras[attribute.slug]}
+                          />
+                        )}
+                      </CCol>
+                    )
+                  })}
+                </CRow>
+
+                {/* Populate the newly added fields */}
+                {!isEmpty(newlyAddedAttributes) ? (
+                  <div>
+                    <HorizontalRule />
+                    <h5 className="font-weight-bold my-4">
+                      Unpopulated Attributes
+                    </h5>
+                    <CRow>
+                      {newlyAddedAttributes.map((field) => (
+                        <CCol xs="12" md="6" lg="4">
+                          {field.datatype === 'enum' ? (
+                            <ComboInput
+                              label={field.name}
+                              name={field.slug}
+                              placeholder={`Enter ${field.name}`}
+                              options={optionsForCheckbox(
+                                field.enum_group.values
+                              )}
+                              // TODO Dont forget to clear the old values.
+                              onChange={(e) => {
+                                _setValuesForProductExtras(field.slug, e.name)
+                              }}
+                              selectValueByLabel={true}
+                              valueByLabel={props.extras[field.slug]}
+                            />
+                          ) : (
+                            <TextField
+                              label={field.name}
+                              name={field.slug}
+                              placeholder={`Enter ${field.name}`}
+                              onChange={(e) => {
+                                _setValuesForProductExtras(
+                                  field.slug,
+                                  e.target.value
+                                )
+                              }}
+                              value={props.extras[field.slug]}
+                            />
                           )}
-                          // TODO Dont forget to clear the old values.
-                          onChange={(e) => {
-                            _setValuesForProductExtras(attribute.slug, e.name)
-                          }}
-                          selectValueByLabel={true}
-                          defaultValueByLabel={value}
-                        />
-                      ) : (
-                        <TextField
-                          label={attribute.name}
-                          name={attribute.slug}
-                          placeholder={`Enter ${attribute.name}`}
-                          onChange={(e) => {
-                            _setValuesForProductExtras(
-                              attribute.slug,
-                              e.target.value
-                            )
-                          }}
-                          value={value}
-                        />
-                      )}
-                    </CCol>
-                  )
-                })}
-              </CRow>
+                        </CCol>
+                      ))}
+                    </CRow>
+                  </div>
+                ) : null}
+              </div>
             ) : (
               <CRow>
                 {fields.map((field) => (
@@ -90,6 +164,7 @@ const AddProductGroupFields = ({
                         onChange={(e) => {
                           _setValuesForProductExtras(field.slug, e.name)
                         }}
+                        selectValueByLabel={true}
                         valueByLabel={props.extras[field.slug]}
                       />
                     ) : (
@@ -100,6 +175,7 @@ const AddProductGroupFields = ({
                         onChange={(e) => {
                           _setValuesForProductExtras(field.slug, e.target.value)
                         }}
+                        value={props.extras[field.slug]}
                       />
                     )}
                   </CCol>
@@ -119,11 +195,13 @@ AddProductGroupFields.defaultProps = {
 
 const mapStateToProps = (state) => {
   return {
-    extrasWithAttributes: state.product.extras_with_attributes,
+    extrasWithAttributes: state.product.extras_with_attributes || [],
     extras: state.product.extras,
+    productGroups: state.root.productGroups,
+    currentProductGroupID: state.product.group,
   }
 }
 
-export default connect(mapStateToProps, { changeProductExtrasInput })(
-  AddProductGroupFields
-)
+export default connect(mapStateToProps, {
+  changeProductExtrasInput,
+})(AddProductGroupFields)
