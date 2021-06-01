6018046d12beadd4f37766b959b47bdbe5ff0fa8
theArjun <be2017se573@gces.edu.np>
2021-06-01 14:26:23 +0545

    Update `requestWrapper`

--- src/api/requestWrapper.js
+++ src/api/requestWrapper.js
@@ -1,30 +1,32 @@
 import { BASE_URL } from '../constants/index'
 
+import Toast from 'src/reusable/Toast/Toast'
+import { ToastMessage } from 'src/reusable/Toast/ToastMessage'
+
 export const requestWrapper = async (url, method, signal, body, header) => {
   try {
-    // if (window.navigator.onLine) {
-    const mainCall = async () => {
-      return await fetch(`${BASE_URL}${url}`, {
-        method: method,
-        signal: signal,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(body),
+    if (window.navigator.onLine) {
+      const mainCall = async () => {
+        return await fetch(`${BASE_URL}${url}`, {
+          method: method,
+          signal: signal,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(body),
+        })
+      }
+      var response = await mainCall()
+      var json = await response.json()
+
+      return { json, response }
+    } else {
+      console.log('the app now is offline okay!')
+      Toast.fire({
+        icon: 'error',
+        title: ToastMessage('error', 'You are offline'),
       })
     }
-    var response = await mainCall()
-    var json = await response.json()
-
-    return { json, response }
-    // } else {
-    //   //no internet connection
-    //   console.log('the app now is offline okay!')
-    //   Toast.fire({
-    //     icon: 'warning',
-    //     title: ToastMessage('warning', 'You are offline'),
-    //   })
-    // }
   } catch (e) {
     throw e
   }
