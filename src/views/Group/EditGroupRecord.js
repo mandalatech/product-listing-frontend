fa8ed1c88f69fd263a695fc43e32cabfef450bef
theArjun <be2017se573@gces.edu.np>
2021-06-02 16:59:03 +0545

    Enhance `EditGroupRecord`

--- src/views/Group/EditGroupRecord.js
+++ src/views/Group/EditGroupRecord.js
@@ -6,6 +6,9 @@ import isEmpty from 'src/validations/isEmpty'
 
 const EditGroupRecord = ({ record }) => {
   const [isEdit, setIsEdit] = useState(false)
+  const [name, setName] = useState('')
+  const [values, setValues] = useState('')
+  const [choices, setChoices] = useState([])
 
   const handleEditChange = (e) => {
     setIsEdit((prevIsEdit) => !prevIsEdit)
@@ -18,22 +21,57 @@ const EditGroupRecord = ({ record }) => {
     } else if (record.datatype === 'enum') {
       values = record.enum_group.values
     }
-    return values
+    return values.map((value) => value.value).join(',')
+  }
+
+  useEffect(() => {
+    // When user clicks cancel, the data needs to be cleared.
+    if (!isEdit) {
+      setName(record.description)
+      setValues(getValues())
+    }
+  }, [isEdit])
+
+  const handleValueChange = (e) => {
+    // Create value payload for saving.
+    const value = e.target.value
+    setValues(value)
+    const __choices = value
+      .split(',')
+      .filter((val) => !isEmpty(val.trim()))
+      .map((val) => {
+        return {
+          value: val.trim(),
+        }
+      })
+    setChoices(__choices)
+  }
+
+  useEffect(() => {
+    setName(record.description)
+    setValues(getValues())
+  }, [])
+
+  const submitPayload = () => {
+    // Write logic for updating attribute.
   }
 
   return (
     <CRow className="d-flex align-items-center">
-      <CCol md="4">
+      <CCol md={isEdit ? '4' : '5'}>
         <TextField
           name="attributeName"
           label="Name"
           placeholder="Attribute Name"
-          value={record.description}
+          value={name}
           plaintext={!isEdit}
           disabled={!isEdit}
+          onChange={(e) => {
+            setName(e.target.value)
+          }}
         />
       </CCol>
-      <CCol md="5">
+      <CCol md={isEdit ? '4' : '5'}>
         <TextField
           name="attributeName"
           label="Possible Values"
@@ -42,20 +80,32 @@ const EditGroupRecord = ({ record }) => {
           }
           plaintext={!isEdit}
           disabled={!isEdit}
-          value={getValues()
-            .map((value) => value.value)
-            .join(',')}
+          value={values}
+          onChange={handleValueChange}
         />
       </CCol>
+      {isEdit ? (
+        <CCol md="2">
+          <CButton
+            color="dark"
+            block={true}
+            variant={isEdit ? '' : 'outline'}
+            style={{ borderRadius: '8%' }}
+            onChange={submitPayload}
+          >
+            Save Changes
+          </CButton>
+        </CCol>
+      ) : null}
       <CCol md="2">
         <CButton
           color="dark"
           block={true}
           onClick={handleEditChange}
-          variant={isEdit ? '' : 'outline'}
+          variant="outline"
           style={{ borderRadius: '8%' }}
         >
-          {isEdit ? 'Save Changes' : 'Edit'}
+          {isEdit ? 'Cancel' : 'Edit'}
         </CButton>
       </CCol>
     </CRow>
